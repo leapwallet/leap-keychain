117fb0479b8a99157ffbdb34c825438dda705e91
baryon2 <opthami@gmail.com>
2024-06-13 11:23:33 +0530

    Update encrypt functions
    1. Remove cryptojs
    2. Use noble ciphers for encryption

--- tests/encryption-utils.test.ts
+++ tests/encryption-utils.test.ts
@@ -1,10 +1,19 @@
 import { decrypt, encrypt } from '../src';
 
 describe('encryption-utils', () => {
-  test('encrypt-decrypt', () => {
+
+  test('encrypt-decrypt-2', () => {
     const originalMessage = 'Hello World';
     const cipher = encrypt(originalMessage, 'password');
     const plain = decrypt(cipher, 'password');
     expect(plain).toBe(originalMessage);
-  });
+  })
+
+
+  test('test with non ascii characters', () => {
+    const originalMessage = 'Hello World! 🐸';
+    const cipher = encrypt(originalMessage, 'password');
+    const plain = decrypt(cipher, 'password');
+    expect(plain).toBe(originalMessage);
+  })
 });
